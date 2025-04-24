4c5093f2fe60cde82b82ac978c17d67f2e763be4
VictorPLopes <victor.probio@gmail.com>
2025-04-23 21:40:02 -0300

    Add Reading record struct

---
+++ VO2MaxMonitor/Models/Reading.cs
@@ -0,0 +1,9 @@
+﻿namespace VO2MaxMonitor.Models;
+
+public readonly record struct Reading(
+    double VenturiAreaRegular,
+    double VenturiAreaConstricted,
+    double O2,
+    double DifferentialPressure,
+    ulong  TimeStamp
+    );
